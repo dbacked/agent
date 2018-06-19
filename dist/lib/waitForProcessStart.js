8680fd0212a96d46aa18f5e2cce5ebbfa141f3f2
Guillaume Besson <guillaume@besson.co>
2018-06-19 13:48:58 +0200

    Added again more verbose output

--- dist/lib/waitForProcessStart.js
+++ dist/lib/waitForProcessStart.js
@@ -1,17 +1,24 @@
 "use strict";
 Object.defineProperty(exports, "__esModule", { value: true });
 const error_1 = require("./error");
+const log_1 = require("./log");
 exports.waitForProcessStart = (childProcess) => {
     return new Promise((resolve, reject) => {
         let processStderr = '';
+        log_1.default.debug('Listening on dump process stderr');
         childProcess.stderr.on('data', (data) => { processStderr += data; });
+        log_1.default.debug('Listening on dump process close event');
         childProcess.on('close', (code) => {
+            log_1.default.debug('Child process close event fired', { code });
             if (code !== 0) {
                 reject(new error_1.DbError(processStderr));
             }
         });
+        log_1.default.debug('Listening on dump process readable event');
         childProcess.stdout.once('readable', () => {
+            log_1.default.debug('Child process readable event fired');
             if (childProcess.stdout.readableLength) {
+                log_1.default.debug('Child process readableLength is > 0', { readableLength: childProcess.stdout.readableLength });
                 resolve();
             }
         });
--- src/lib/waitForProcessStart.ts
+++ src/lib/waitForProcessStart.ts
@@ -1,17 +1,24 @@
 import { DbError } from './error';
+import logger from './log';
 
 
 export const waitForProcessStart = (childProcess) => {
   return new Promise((resolve, reject) => {
     let processStderr = '';
+    logger.debug('Listening on dump process stderr');
     childProcess.stderr.on('data', (data) => { processStderr += data; });
+    logger.debug('Listening on dump process close event');
     childProcess.on('close', (code) => {
+      logger.debug('Child process close event fired', { code });
       if (code !== 0) {
         reject(new DbError(processStderr));
       }
     });
+    logger.debug('Listening on dump process readable event');
     childProcess.stdout.once('readable', () => {
+      logger.debug('Child process readable event fired');
       if (childProcess.stdout.readableLength) {
+        logger.debug('Child process readableLength is > 0', { readableLength: childProcess.stdout.readableLength });
         resolve();
       }
     });
