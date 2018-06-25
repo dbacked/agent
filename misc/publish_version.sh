622ab52dfdc5e0dcec30557990ddefcd85c677b2
Guillaume Besson <guillaume@besson.co>
2018-06-25 15:56:33 +0200

    Fixed S3 acl

--- misc/publish_version.sh
+++ misc/publish_version.sh
@@ -13,5 +13,5 @@ docker build . -t geekuillaume/dbacked:latest
 docker tag geekuillaume/dbacked:latest geekuillaume/dbacked:$PACKAGE_VERSION
 docker push geekuillaume/dbacked:latest
 docker push geekuillaume/dbacked:$PACKAGE_VERSION
-aws s3 cp dbacked s3://dbacked
-aws s3 cp dbacked_md5 s3://dbacked
+aws s3 cp dbacked s3://dbacked --acl public-read
+aws s3 cp dbacked_md5 s3://dbacked --acl public-read
